44f9f40d7d5d66c51b07b805cd459c025558ff30
User7575 <2589391494@qq.com>
2023-12-11 16:42:15 +0800

    [新增]编译原理

---
+++ demoMakefile/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int printString()
+{
+    int len = 0;
+    printf("print String\n");
+    return len;
+}
+int main()
+{
+
+    int num1 = 10;
+    printf("num1:%d\n",num1);
+    printString();
+
+
+
+
+
+    return 0;
+
+}
